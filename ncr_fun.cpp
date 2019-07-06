779c1cb44f18363b92b3b29c7cff469fa031b326
emadhashem20 <47951503+emadhashem20@users.noreply.github.com>
2019-07-06 17:39:44 +0200

    Create ncr_fun.cpp

---
+++ ncr_fun.cpp
@@ -0,0 +1,11 @@
+ll n_c(ll n, ll k) {
+    if (k * 2 > n) k = n-k;
+    if (k == 0) return 1;
+
+    ll result = n;
+    for( int i = 2; i <= k; ++i ) {
+        result *= (n-i+1);
+        result /= i;
+    }
+    return result;
+}
