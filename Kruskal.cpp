30fcf13a34d7ee4605cc9bb8548c595628d30642
emadhashem20 <47951503+emadhashem20@users.noreply.github.com>
2019-09-06 01:33:33 +0200

    Update Kruskal.cpp

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -1,70 +1,68 @@
 #include <bits/stdc++.h>
+
+#define MAX 1010
+
 using namespace std;
 
+vector <pair<int, pair<int, int> > > edges;
+int parent[MAX], _rank[MAX];
 
-typedef long long ll;
-
-const int MAXN = 1e6+6; // max number of nodes.
-
-struct edge{
-    int from, to;
-    ll cost;
-    bool operator < (const edge &e) const {
-        return  cost < e.cost;
-    }
-};
-
-struct DSU {
-
-    int id[MAXN], sz[MAXN];
-
-    DSU() {
-        for(int i = 0; i < MAXN; i++) {
-            id[i] = i, sz[i] = 1;
-        }
-    }
-
-    int root(int i) {
-        while(i != id[i]) {
-            i = id[i] = id[id[i]];
-        }
-        return i;
-    }
-
-    bool unite(int a , int b) {
-        a = root(a), b = root(b);
-        if(a == b) return 0;
-        if (sz[a] < sz[b]) swap(a, b);
-        if (sz[a] == sz[b]) ++sz[a];
-        id[b] = a;
-        return 1;
-    }
-};
-
-
-ll kruskal(vector<edge> edgeList) { // edges and number of nodes
-    DSU D;
-    ll mstCost = 0;
-    sort(edgeList.begin(), edgeList.end());
-    for(int i = 0; i < (int)edgeList.size(); i++) {
-        if(D.unite(edgeList[i].from, edgeList[i].to)) {
-            mstCost += edgeList[i].cost;
-        }
-    }
-    /*
-        if(number of edges != number of nodes - 1
-            then there is no mst
-    */
-    return mstCost;
+int find_set(int x){
+    if(x == parent[x]) return x;
+    return parent[x] = find_set(parent[x]);
 }
 
-int main () {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    vector<edge> ve(m);
-    for(int i = 0; i < m; i++) {
-        scanf("%d %d %lld", &ve[i].from, &ve[i].to, &ve[i].cost);
-    }
-    printf("%lld\n", kruskal(ve));
+bool union_set(int x, int y){
+    int i = find_set(x),
+        j = find_set(y);
 
+    if(i != j){
+        if(_rank[i] > _rank[j]){
+            parent[j] = i;
+            _rank[i] += _rank[j];
+        }else{
+            parent[i] = j;
+            _rank[j] += _rank[i];
+        }
+
+        return true;
+    }
+    return false;
+}
+
+int kruskal(int v, int e){
+    int mst = 0, k = 0, w, x, y;
+    
+    sort(edges.begin(), edges.end());
+
+    for(int i = 0; i < e && k < v - 1; i++){
+        w = edges[i].first;
+        x = edges[i].second.first, y = edges[i].second.second;
+
+        if(union_set(x, y)){
+            mst += w;
+            k++;
+        }
+    }
+    
+    return mst;
+}
+
+int main(){
+
+    int v, e, x, y, w;
+
+    for(int i = 1; i <= v; i++){
+        _rank[i] = 0;
+        parent[i] = i;
+    }
+
+    while(e--){
+        scanf("%d %d %d", &x, &y, &w);
+        edges.push_back(make_pair(w, make_pair(x, y)));
+    }
+
+    kruskal(v, e);
+
+    return 0;
 }
